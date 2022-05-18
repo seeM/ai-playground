0358dfadf2adfb32e697799670f9204564dc00b1
Wasim Lorgat <mwlorgat@gmail.com>
2022-05-18 10:17:12 +0000

    working finetuning with mmdetection

--- object-detection/fruit_detection.py
+++ object-detection/fruit_detection.py
@@ -1,21 +1,44 @@
 _base_ = 'mmdetection/configs/mask_rcnn/mask_rcnn_r50_caffe_fpn_mstrain-poly_1x_coco.py'
 
+custom_imports = dict(imports=['mmdet.datasets.mineapple',
+                               'mmdet.datasets.pipelines.mineapple'], allow_failed_imports=False)
+classes = ('fruit',)
+num_classes = len(classes)
 model = dict(
     roi_head=dict(
-        bbox_head=dict(num_classes=1),
-        mask_head=dict(num_classes=1)))
+        bbox_head=dict(num_classes=num_classes),
+        mask_head=dict(num_classes=num_classes)))
 
-dataset_type = 'COCODataset'
-classes = ('balloon',)
 dataset = dict(
     _delete_=True,
     type="MineAppleDataset",
-    classes=("fruit",),
-    data_root="data/")
+    classes=classes,
+    data_root="../data/")
+img_norm_cfg = dict(
+    mean=[103.530, 116.280, 123.675], std=[1.0, 1.0, 1.0], to_rgb=False)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='LoadMineappleAnnotations',
+        with_bbox=True,
+        with_mask=True,
+        poly2mask=False),
+    dict(
+        type='Resize',
+        img_scale=[(1333, 640), (1333, 672), (1333, 704), (1333, 736),
+                   (1333, 768), (1333, 800)],
+        multiscale_mode='value',
+        keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks']),
+]
 data = dict(
     train=dict(
         **dataset,
-        pipeline={{ _base_.data.train.pipeline }}),
+        pipeline=train_pipeline),
     val=dict(
         **dataset,
         pipeline={{ _base_.data.val.pipeline }}),
@@ -24,4 +47,18 @@ data = dict(
         test_mode=True,
         pipeline={{ _base_.data.test.pipeline }}))
 
-load_from = 'checkpoints/mask_rcnn_r50_caffe_fpn_mstrain-poly_3x_coco_bbox_mAP-0.408__segm_mAP-0.37_20200504_163245-42aa3d00.pth'
+# lr is set for a batch size of 8
+optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
+optimizer_config = dict(grad_clip=None)
+# learning policy
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=500,
+    warmup_ratio=0.001,
+    step=[7])
+# the max_epochs and step in lr_config need specifically tuned for the customized dataset
+runner = dict(max_epochs=1)
+log_config = dict(interval=50)
+
+load_from = 'https://download.openmmlab.com/mmdetection/v2.0/mask_rcnn/mask_rcnn_r50_caffe_fpn_mstrain-poly_3x_coco/mask_rcnn_r50_caffe_fpn_mstrain-poly_3x_coco_bbox_mAP-0.408__segm_mAP-0.37_20200504_163245-42aa3d00.pth'
